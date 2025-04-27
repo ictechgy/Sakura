68ea603796baf72961085906951054a8cd8b9eb2
ictechgy <ictechgy@gmail.com>
2025-04-28 00:03:31 +0900

    [feat] 하위 컨트랙트 구현

--- contracts/Sakura.sol
+++ contracts/Sakura.sol
@@ -32,6 +32,50 @@ contract Sakura is ERC20, Ownable {
         leaf.createSurvey(_question, _options);
     }
 
-    // 특정 설문조사 삭제 및 CRUD 필요
+    // TODO: 특정 설문조사 삭제 및 CRUD 필요
 }
 
+// 하위 컨트랙트
+contract Leaf is Ownable {
+    struct Survey {
+        string question;
+        string[] options;
+        uint[] voteCountPerOptions;
+        bool isActive;
+    }
+
+    Survey[] surveys;
+
+    // 설문조사 생성
+    function createSurvey(string memory _question, string[] memory _options, bool _initialActiveState) public onlyOwner {
+        require(_options.length >= 2, "At least 2 options are required");
+        uint[] memory initialVotes = new uint[](_options.length);
+        surveys.push(
+            Survey(
+                {
+                    question: _question,
+                    options: _options,
+                    votes: initialVotes,
+                    active: _initialActiveState
+                }
+            )
+        );
+    }
+
+    // 투표 함수
+    function vote(uint _surveyId, uint _optionIndex) public {
+        require(_surveyId < surveys.length, "Survey does not exist");
+        require(surveys[_surveyId].active, "Survey is not active");
+        require(_optionIndex < surveys[_surveyId].options.length, "Invalid option index");
+        surveys[_surveyId].votes[_optionIndex]++;
+        // TODO: 사용자 체크 필요. msg.sender
+    }
+
+    // 설문조사 결과 보기
+    function getSurveyResults(uint _surveyId) public view returns (string memory, string[] memory, uint[] memory, bool) {
+        require(_surveyId < surveys.length, "Survey does not exist");
+        Survey memory survey = surveys[_surveyId];
+
+        return (survey.question, survey.options, survey.voteCountPerOptions, survey.isActive)
+    }
+}
