a87558d132cccd13987a4148ccc10d960794d97a
ictechgy <ictechgy@gmail.com>
2025-05-19 00:06:34 +0900

    [fix] import 구문 추가

--- contracts/Soboro.sol
+++ contracts/Soboro.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20Burnable
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardTransientUpgradeable.sol";
+import "./Crumb.sol";
 
 // 메인 컨트랙트 선언
 contract Soboro is Initializable, ERC20Upgradeable, ERC20CappedUpgradeable, ERC20BurnableUpgradeable, OwnableUpgradeable, ReentrancyGuardTransientUpgradeable {
