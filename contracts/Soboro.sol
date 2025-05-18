0528a1ce77498a742a29de94e14fb5d04d8c60a6
ictechgy <ictechgy@gmail.com>
2025-05-18 23:08:01 +0900

    [refactor] _update 함수에서 owner체크 제거

--- contracts/Soboro.sol
+++ contracts/Soboro.sol
@@ -28,7 +28,6 @@ contract Soboro is Initializable, ERC20Upgradeable, ERC20CappedUpgradeable, ERC2
     }
 
     function _update(address from, address to, uint256 value) internal override(ERC20Upgradeable, ERC20CappedUpgradeable) {
-        _checkOwner();
         super._update(from, to, value);
     }
 
