ec385faf33f0cda5470bb53eabb2322986cbd676
ictechgy <ictechgy@gmail.com>
2025-05-18 22:57:31 +0900

    [fix] Soboro > _update 함수 override

--- contracts/Soboro.sol
+++ contracts/Soboro.sol
@@ -27,6 +27,11 @@ contract Soboro is Initializable, ERC20Upgradeable, ERC20CappedUpgradeable, ERC2
         _mint(msg.sender, 10**6);
     }
 
+        function _update(address from, address to, uint256 value) internal override(ERC20Upgradeable, ERC20CappedUpgradeable) {
+        _checkOwner();
+        super._update(from, to, value);
+    }
+
     // 새로운 서브 컨트랙트를 생성
     function createCrumbContract() public onlyOwner {
         require(crumbMap[crumbGenID] == address(0), "crumb aleady baked");
